f7539978e0e5d7d5fef499406f9ce0a1e747481c
Lukasz Malucha <lucasmalucha@gmail.com>
2025-11-10 12:33:22 +0000

    component

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png"
+import CortanaImage from "./images/cortana.png"
+import SiriImage from "./images/siri.png"
+
+function App() {
+    return (
+    <div>
+        <div>Personal Digital Assistant</div>
+
+        <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+        <ProfileCard title="Cortana" handle="@cortana12" image={CortanaImage} />
+        <ProfileCard title="Siri" handle="@siri24" image={SiriImage} />
+    </div>
+
+
+    );
+
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
 import { React } from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
+
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
 
-function App() {
-    const message = "Bye There"
 
-    return <h1>{ message }</h1>;
-}
 
 root.render(<App />);
