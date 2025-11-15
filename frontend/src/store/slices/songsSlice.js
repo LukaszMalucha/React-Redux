5b63cb701fa4d6c0527c8d058ec7622ba52eeb56
Lukasz Malucha <lucasmalucha@gmail.com>
2025-11-15 14:10:32 +0000

    refactor

---
+++ frontend/src/store/slices/songsSlice.js
@@ -0,0 +1,46 @@
+import {createSlice, createAction } from "@reduxjs/toolkit";
+
+export const reset = createAction("app/reset");
+
+const songsSlice = createSlice({
+    name: "song",
+    initialState: [],
+    reducers: {
+        addSong(state, action) {
+            state.push(action.payload);
+        },
+        removeSong(state, action) {
+            const index = state.indexOf(action.payload);
+            state.splice(index, 1);
+        },
+    },
+    extraReducers(builder) {
+      builder.addCase(reset, (state, action) => {
+        return [];
+      });
+    }
+});
+
+
+export const { addSong, removeSong } = songsSlice.actions;
+export const songsReducer = songsSlice.reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
